1b995911d557aec240ec0b7df269257dfbc18a12
Alejo Totaro <alejocs1998@gmail.com>
2021-06-07 15:35:31 -0300

    .

--- api/src/routes/country.js
+++ api/src/routes/country.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
-const axios = require('axios')
 const { Sequelize } = require("sequelize");
-const { Country, Activity, Season } = require('../db');
+const { Country, Activity} = require('../db');
 
 router.get('/', async (req, res) =>{
     
