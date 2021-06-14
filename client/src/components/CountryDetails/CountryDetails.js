74d7492b66c6f3d558bc92b9af8191deae6cac04
Alejo Totaro <alejocs1998@gmail.com>
2021-06-14 11:29:42 -0300

    .

--- client/src/components/CountryDetails/CountryDetails.js
+++ client/src/components/CountryDetails/CountryDetails.js
@@ -19,7 +19,6 @@ const CountrDetails = () => {
     // function formatNumber(num) {
     //     return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
     //   }
-
     return (
         
        <div className="Details-Content">
--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -13,7 +13,7 @@ const LandingPage = () => {
                 <div className="Margin-LandingPage">
                 <h1 className="Title-LandingPage">Welcome To Countries</h1>
                 <p className="Info-LandingPage">Here you can Search any Country and Add Activities him.</p>
-                <Link to="NavBar" spy={true} smooth={true}><img className="bounce" src={Arrow}></img></Link>
+                <Link to="NavBar" spy={true} smooth={true}><img className="bounce" alt="arrow" src={Arrow}></img></Link>
                 {/* <Link to="/home">
                 
                 <img className="bounce" src={Arrow}></img>
